30c96c8c9505bcf9ede4c28212d6bd4e04a05144
Vladimir <vovnik98@mail.ru>
2024-02-15 22:55:14 +0300

    sprint_5 исправление замечаний

--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -5,7 +5,7 @@ from locators import *
 
 class TestPersonalAccount:
     def test_switch_to_personal_account(self, driver, login):
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LocatorsMainPage.BTN_MAKE_ORDER))
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LocatorsMainPage.BTN_MY_ACNT))
         driver.find_element(*LocatorsMainPage.BTN_MY_ACNT).click()
         # Проверяем соответствие логина в профиле
         assert WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LocatorsPersonalAccounts.USER_NAME))
